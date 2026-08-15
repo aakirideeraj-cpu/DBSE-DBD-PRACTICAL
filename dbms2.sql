a6e7b89843896c084f551b3aa11f14ebb350b750
aakirideeraj-cpu <aakirideeraj@gmail.com>
2026-08-15 20:39:59 +0530

    Add files via upload

---
+++ dbms2.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE bankdb;
+USE bankdb;
+
+CREATE TABLE bank_transactions (
+    txn_id INT PRIMARY KEY,
+    customer_name VARCHAR(50),
+    branch_name VARCHAR(50),
+    transaction_type VARCHAR(20),
+    amount DECIMAL(10,2),
+    transaction_date DATE
+);
+INSERT INTO bank_transactions VALUES
+(101,'Ravi','Hyderabad','Deposit',5000,'2024-01-05'),
+(102,'Sita','Hyderabad','Withdrawal',2000,'2024-01-06'),
+(103,'Kiran','Vijayawada','Deposit',12000,'2024-01-08'),
+(104,'Anil','Vizag','Deposit',8000,'2024-01-10'),
+(105,'Priya','Hyderabad','Withdrawal',3500,'2024-01-11'),
+(106,'Ramesh','Vizag','Deposit',15000,'2024-01-12'),
+(107,'Keerthi','Vijayawada','Withdrawal',1000,'2024-01-13'),
+(108,'Rahul','Hyderabad','Deposit',9000,'2024-01-14'),
+(109,'Sneha','Vizag','Withdrawal',4000,'2024-01-15'),
+(110,'Madhu','Vijayawada','Deposit',11000,'2024-01-16');
+
+SELECT * FROM bank_transactions;
